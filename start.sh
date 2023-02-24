20933fb4288b8c16ddc3fce18376f1c48dbcf1c1
ʜᴀʀɪ <124697828+HS-BOTS@users.noreply.github.com>
2023-02-24 18:57:55 +0530

    🍁𝙃𝙎-𝘽𝙊𝙏-𝙑4🍁

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/HS-BOTS/HS-AUTO-FILTER.git /HS-AUTO-FILTER
+  git clone https://github.com/HS-BOTS/AUTO-FILTER-BOT-V4.git /AUTO-FILTER-BOT-V4
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /HS-AUTO-FILTER
+  git clone $UPSTREAM_REPO /AUTO-FILTER-BOT-V4
 fi
-cd /HS-AUTO-FILTER
+cd /AUTO-FILTER-BOT-V4
 pip3 install -U -r requirements.txt
-echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖....🧞‍♂️"
+echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝘼𝙐𝙏𝙊-𝙁𝙄𝙇𝙏𝙀𝙍-𝘽𝙊𝙏-𝙑4....❣️💌"
 python3 bot.py
