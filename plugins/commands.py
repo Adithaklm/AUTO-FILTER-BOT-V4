ad7ab7e50d362349f12f54bf6a8d7a8485719c41
Adithak_99 <94104632+Adithaklm@users.noreply.github.com>
2023-05-18 19:41:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -56,9 +56,6 @@ async def start(client, message):
             InlineKeyboardButton('〆 ꜱᴜᴩᴩᴏʀᴛ ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ 〆', url='https://t.me/TAMIL_FLIMS_HD')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("CAACAgQAAxkBAAIEgGP5cawhK82upPWHbZHgTG8LDCtwAAINDwACqxrJU7hVOwpIQxetHgQ") 
-        await asyncio.sleep(1)
-        await m.delete()        
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.SUR_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
